01cc0ddef1acc7241601d48638c932e8a8ab0d54
leo <61714687+leopoldo-1@users.noreply.github.com>
2022-03-24 14:42:47 -0700

    First Commit
    User registration added

---
+++ scripts/list.js
@@ -0,0 +1,21 @@
+console.log('list...')
+
+function displayUsers() {
+  //travel the array
+
+
+  //get each user
+  // []
+
+  //display the user
+
+  //append the user to the table append() from jquery
+}
+
+function init() {
+  console.log('listening users');
+  let users = readuUsers();
+  displayUsers(users)
+}
+
+window.onload = init;
