d8bdeab17386c5acf2742f815351a180513fd4ea
leo <61714687+leopoldo-1@users.noreply.github.com>
2022-03-24 16:56:18 -0700

    105-assignment-3

--- scripts/list.js
+++ scripts/list.js
@@ -1,20 +1,40 @@
 console.log('list...')
 
-function displayUsers() {
+function displayUsers(users) {
   //travel the array
-
+  let tabla = $('#tableUsers').val()
+  let template = ""
+  console.log('... ', users);
 
   //get each user
-  // []
-
-  //display the user
+  for (let i = 0; i < users.length; i++) {
+    template +=
+      `
+      <tr>
+        <td>${users[i].firstName}</td>
+        <td>${users[i].lastName}</td>
+        <td>${users[i].email}</td>
+        <td>${users[i].age}</td>
+        <td>${users[i].address}</td>
+        <td>${users[i].cardNumber}</td>
+        <td>${users[i].contactPhone}</td>
+        <td><input type="color"> ${users[i].color}</input></td>
+      </tr>
+    `
+  }
 
   //append the user to the table append() from jquery
+  $('#tableUsers').append(template)
 }
 
 function init() {
   console.log('listening users');
-  let users = readuUsers();
+  let users = readUsers();
+
+
+  // let inputFirstName = $('#txtFirstName').val();
+
+
   displayUsers(users)
 }
 
